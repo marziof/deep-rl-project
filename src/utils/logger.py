b1ea2d48a91a5afaf9f6b8e9fb9bdbcdf10e7173
Marzio Formica <marzio.formica@epfl.ch>
2026-04-27 11:52:12 +0200

    Implemented minimal logger class

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,1 +1,32 @@
 # File to log training data
+
+import numpy as np
+
+
+class Logger:
+    def __init__(self):
+        self.episode_rewards = []
+        self.eval_rewards = []
+        self.losses = []
+        self.epsilons = []
+
+    def reset(self):
+        self.episode_rewards = []
+        self.eval_rewards = []
+        self.losses = []
+        self.epsilons = []
+
+    def log_episode_reward(self, r):
+        self.episode_rewards.append(r)
+
+    def log_eval_reward(self, r):
+        self.eval_rewards.append(r)
+
+    def log_loss(self, loss):
+        self.losses.append(loss)
+
+    def log_epsilon(self, eps):
+        self.epsilons.append(eps)
+
+    def moving_average(self, window=100):
+        return np.convolve(self.episode_rewards, np.ones(window)/window, mode='valid')
